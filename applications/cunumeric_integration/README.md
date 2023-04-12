2dd8dee5bcf423db573c8da8a36cef2c6dc3ea1f
Adam Thompson <34922235+awthomp@users.noreply.github.com>
2023-04-12 09:05:32 -0400

    Add Holoscan + cuNumeric Example  (#17)
    * cunumeric example with holoscan to target multi-gpu python numerical computing

---
+++ applications/cunumeric_integration/README.md
@@ -0,0 +1,32 @@
+# Calculate Power Spectral Density with Holoscan and cuNumeric
+
+[cuNumeric](https://github.com/nv-legate/cunumeric) is an drop-in replacement for NumPy that aims to provide a distributed and accelerated drop-in replacement for the NumPy API on top of the [Legion](https://legion.stanford.edu/) runtime. It works best for programs that have very large arrays of data that can't fit in the the memory of a single GPU or node.
+
+In this example application, we are using the cuNumeric library within a Holoscan application graph to determine the Power Spectral Density (PSD) of an incoming signal waveform. Notably, this is simply achived by taking the absolute value of the FFT of a data array.
+
+ The main objectives of this demonstration are to:
+- Highlight developer productivity in building an end-to-end streaming application with Holoscan and cuNumeric
+- Demonstrate how to scale a given workload to multiple GPUs using cuNumeric
+
+# Running the Application
+
+Prior to running the application, the user needs to install the necessary dependencies. This is most easily done in an Anaconda environment.
+
+```
+conda create --name holoscan-cunumeric-demo python=3.9
+conda activate holoscan-cunumeric-demo
+conda install -c nvidia -c conda-forge -c legate cunumeric cupy
+pip install holoscan
+```
+
+The cuNumeric PSD processing pipeline example can then be run via
+```
+legate --gpus 2 applications/cunumeric_integration/cunumeric_psd.py
+```
+
+While running the application, you can confirm multi GPU utilization via watching `nvidia-smi` or using another GPU utilization tool
+
+To run the same application without cuNumeric, simply change `import cunumeric as np` to `import cupy as np` in the code and run
+```
+python applications/cunumeric_integration/cunumeric_psd.py
+```
---
+++ applications/cunumeric_integration/cunumeric-psd.py
@@ -0,0 +1,78 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from holoscan.conditions import CountCondition
+from holoscan.core import Application, Operator, OperatorSpec
+from holoscan.logger import load_env_log_level
+
+import cunumeric as np
+
+class SourceOp(Operator):
+    def __init__(self, *args, **kwargs):
+        self.fs = 10e3  # sample rate
+        self.N = 1e7  # number of samples
+        self.t = np.arange(self.N) / float(self.fs) # array of segment times
+        self.signal = np.sin(2*np.pi*self.t)
+    
+        super().__init__(*args, **kwargs)
+        
+    def setup(self, spec: OperatorSpec):
+        spec.output("signal_out")
+        
+    def compute(self, op_input, op_output, context):
+        op_output.emit(self.signal, "signal_out")
+
+        
+class PSDOp(Operator):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def setup(self, spec: OperatorSpec):
+        spec.input("signal_in")
+        spec.output("psd_out")
+
+    def compute(self, op_input, op_output, context):
+        sig = op_input.receive("signal_in")
+        op_output.emit(np.abs(np.fft.fft(sig)), "psd_out")
+
+        
+class SinkOp(Operator):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def setup(self, spec: OperatorSpec):
+        spec.input("psd_in")
+        
+    def compute(self, op_input, op_output, context):
+        sig = op_input.receive("psd_in")
+        print(sig[0:5])
+
+
+class PSDApp(Application):
+    def compose(self):
+        src = SourceOp(self, CountCondition(self, 10), name="src_op")
+        psd = PSDOp(self, name="psd_op")
+        sink = SinkOp(self, name="sink_op")
+
+        # Connect the operators into the workflow:  src -> psd -> sink
+        self.add_flow(src, psd)
+        self.add_flow(psd, sink)
+        
+
+if __name__ == "__main__":
+    load_env_log_level()
+    app = PSDApp()
+    app.config("")
+    app.run()
