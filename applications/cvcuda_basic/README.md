de8190dab21036f371e078c6f2236ab14d4a976a
Gregory Lee <grlee77@gmail.com>
2023-08-25 11:30:15 -0400

    check in cvcuda_basic.py example

---
+++ applications/cvcuda_basic/README.md
@@ -0,0 +1,10 @@
+# Simple CV-CUDA application
+
+This application demonstrates seamless interoperability between Holoscan tensors and CV-CUDA tensors. The image processing pipeline is just a simple flip of the video orientation.
+
+# Running the Application
+
+The simple CV-CUDA pipeline example can then be run via
+```
+python applications/cvcuda_basic/python/cvcuda_basic.py --data=/workspace/holohub/data/endoscopy
+```
---
+++ applications/cvcuda_basic/python/cvcuda_basic.py
@@ -0,0 +1,150 @@
+"""
+SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+SPDX-License-Identifier: Apache-2.0
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""  # no qa
+
+import os
+import sys
+from argparse import ArgumentParser
+
+from holoscan.core import Application, Operator, OperatorSpec
+from holoscan.gxf import Entity
+from holoscan.operators import HolovizOp, VideoStreamReplayerOp
+
+import cvcuda
+import nvcv
+
+try:
+    import cupy as cp
+    import cupyx.scipy.ndimage as ndi
+except ImportError:
+    raise ImportError(
+        "CuPy must be installed to run this example. See "
+        "https://docs.cupy.dev/en/stable/install.html"
+    )
+
+
+# Define custom Operators for use in the demo
+class ImageProcessingOp(Operator):
+    """Example of an operator processing input video (as a tensor).
+
+    This operator has:
+        inputs:  "input_tensor"
+        outputs: "output_tensor"
+
+    The data from each input is processed by a CuPy gaussian filter and
+    the result is sent to the output.
+
+    In this demo, the input and output image (2D RGB) is a 3D array of shape
+    (height, width, channels).
+    """
+
+    def __init__(self, fragment, *args, use_flip_into=False, **kwargs):
+        self.count = 1
+        self.cv_out = None
+
+        # Need to call the base class constructor last
+        super().__init__(fragment, *args, **kwargs)
+
+    def setup(self, spec: OperatorSpec):
+        spec.input("input_tensor")
+        spec.output("output_tensor")
+
+    def compute(self, op_input, op_output, context):
+        tensormap = op_input.receive("input_tensor")
+        input_tensor = tensormap[""]  # stride (2562, 3, 1)
+
+        print(f"message received (count: {self.count})")
+        self.count += 1
+
+        cv_in = nvcv.as_tensor(input_tensor, "HWC")  # Input_tensor is (480, 854, 3)
+
+        if self.cv_out is None:
+            # store output buffer for future reuse
+            self.cv_out = cvcuda.flip(src=cv_in, flipCode=0)
+        else:
+            # reuse the buffer from the first compute call
+            cvcuda.flip_into(src=cv_in, dst=self.cv_out, flipCode=0)
+
+        buffer = self.cv_out.cuda()
+        output_tensormap = dict(image=buffer)
+        op_output.emit(output_tensormap, "output_tensor")
+
+
+# Now define a simple application using the operators defined above
+class MyVideoProcessingApp(Application):
+    """Example of an application that uses the operators defined above.
+
+    This application has the following operators:
+
+    - VideoStreamReplayerOp
+    - ImageProcessingOp
+    - HolovizOp
+
+    The VideoStreamReplayerOp reads a video file and sends the frames to the ImageProcessingOp.
+    The ImageProcessingOp processes the frames and sends the processed frames to the HolovizOp.
+    The HolovizOp displays the processed frames.
+    """
+    def __init__(self, data):
+        super().__init__()
+
+        if data == "none":
+            data = os.path.join(os.environ.get("HOLOSCAN_DATA_PATH", "../data"), "endoscopy")
+
+        self.sample_data_path = data
+
+    def compose(self):
+        width = 854
+        height = 480
+        video_dir = self.sample_data_path
+        if not os.path.exists(video_dir):
+            raise ValueError(f"Could not find video data: {video_dir=}")
+        source = VideoStreamReplayerOp(
+            self,
+            name="replayer",
+            directory=video_dir,
+            basename="surgical_video",
+            frame_rate=0,
+            repeat=True,
+            realtime=True,
+            count=0,
+        )
+
+        image_processing = ImageProcessingOp(self, name="image_processing")
+
+        visualizer = HolovizOp(
+            app,
+            name="holoviz",
+            width=width,
+            height=height,
+            tensors=[dict(name="image", type="color", opacity=1.0, priority=0)],
+        )
+
+        app.add_flow(source, image_processing)
+        app.add_flow(image_processing, visualizer, {("output_tensor", "receivers")})
+
+
+if __name__ == "__main__":
+    # Parse args
+    parser = ArgumentParser(description="CV-CUDA demo application.")
+    parser.add_argument(
+        "-d",
+        "--data",
+        default="none",
+        help=("Set the data path"),
+    )
+    args = parser.parse_args()
+    app = MyVideoProcessingApp(data=args.data)
+    app.run()
