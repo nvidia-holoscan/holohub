6dcda489473ae3593fb4e03bc37ad407703fcef7
Julien Jomier <jjomier@nvidia.com>
2023-09-19 09:59:12 -0400

    Added script for generation of pt model

---
+++ applications/object_detection_torch/generate_resnet_model.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import os
+
+import torch
+from torchvision.models import detection
+
+os.environ['TORCH_HOME'] = os.getcwd()
+
+model_file = "frcnn_resnet50_t.pt"
+if len(sys.argv) > 1:
+    model_file = sys.argv[1]
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+det_model = detection.fasterrcnn_resnet50_fpn(progress=True).to(DEVICE)
+
+det_model.eval()
+det_model_script = torch.jit.script(det_model)
+det_model_script.save(model_file)
