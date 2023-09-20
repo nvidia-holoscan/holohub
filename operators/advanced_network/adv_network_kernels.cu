d5e1fbe6c4a122a6654916106cb7da807c77d978
Cliff Burdick <30670611+cliffburdick@users.noreply.github.com>
2023-09-20 09:44:33 -0700

    Update adv_network_kernels.cu
    Fixed incorrect access on reorder kernel
    
    Signed-off-by: Cliff Burdick <30670611+cliffburdick@users.noreply.github.com>

--- operators/advanced_network/adv_network_kernels.cu
+++ operators/advanced_network/adv_network_kernels.cu
@@ -37,8 +37,8 @@ __global__ void simple_packet_reorder_kernel(void * __restrict__ out,
   if (pkt_idx < num_pkts) {
     for (int pos = threadIdx.x; pos < len / 4; pos += blockDim.x) {
       const uint32_t *in_ptr  = static_cast<const uint32_t*>(in_pkt) + pos;
-      uint32_t *out_ptr       = static_cast<uint32_t*>(out)    + pos;
-      out_ptr[pos]            = in_ptr[pos];
+      uint32_t *out_ptr       = (uint32_t*)((uint8_t *)out + pkt_idx * pkt_len)  + pos;
+      *out_ptr                = *in_ptr;
     }
   }
 }
