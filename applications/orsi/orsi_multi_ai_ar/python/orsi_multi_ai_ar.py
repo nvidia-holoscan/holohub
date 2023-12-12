47f04477f6032fc3f7628501a05cc43d0e56429a
Julien Jomier <jjomier@nvidia.com>
2023-12-12 12:47:52 -0500

    Fixed CMake and testing via ctest and renamed configuration files to match current HoloHub
    Signed-off-by: Julien Jomier <jjomier@nvidia.com>

--- applications/orsi/orsi_multi_ai_ar/python/orsi_multi_ai_ar.py
+++ applications/orsi/orsi_multi_ai_ar/python/orsi_multi_ai_ar.py
@@ -1,3 +1,17 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 
 from holoscan.core import Application
@@ -33,8 +47,8 @@ class OrsiMultiAIARApp(Application):
             name="multiai_inference",
             allocator=allocator,
             model_path_map={
-                "tool_segmentation": self.data_path + "/model/segmentation_model.onnx",
-                "anonymization": self.data_path + "/model/anonymization_model.onnx",
+                "tool_segmentation": self.data_path + "/models/segmentation_model.onnx",
+                "anonymization": self.data_path + "/models/anonymization_model.onnx",
             },
             **self.kwargs("multiai_inference"),
         )
@@ -93,7 +107,7 @@ class OrsiMultiAIARApp(Application):
 def main():
     set_log_level(LogLevel.WARN)
     app = OrsiMultiAIARApp()
-    config_file = os.path.join(os.path.dirname(__file__), "app_config.yaml")
+    config_file = os.path.join(os.path.dirname(__file__), "orsi_multi_ai_ar.yaml")
     app.config(config_file)
     app.run()
 
