0dc5621493efd5f66f27e9a98c2f218886e4d021
Nigel Nelson <61555377+NigelNelson@users.noreply.github.com>
2024-03-14 14:44:03 -0700

    Fix lint errors
    Signed-off-by: Nigel Nelson <61555377+NigelNelson@users.noreply.github.com>

--- applications/holochat_local/holochat/llm.py
+++ applications/holochat_local/holochat/llm.py
@@ -53,9 +53,7 @@ class LLM:
 
     def answer_question(self, chat_history):
         question = chat_history[-1][0]
-        docs = self.db.similarity_search_with_score(
-            query=question, k=NUM_HOLOSCAN_DOCS
-        )
+        docs = self.db.similarity_search_with_score(query=question, k=NUM_HOLOSCAN_DOCS)
         # Filter out poor matches from vector db
         docs = list(
             map(lambda lc_doc: lc_doc[0], filter(lambda lc_doc: lc_doc[1] < SEARCH_THRESHOLD, docs))
