a92a70f2e00d7299fe0e5eb6a73b2552a85cded8
Tom Birdsong <tbirdsong@nvidia.com>
2024-04-16 16:20:03 -0400

    App: Update `volume_rendering_xr` for Magic Leap 2 v1.5.0
    Changes:
    - Updates README versions and instructions for using newer Magic
      Leap 2 firmware (v1.5.0) and accompanying remote rendering software
      to view volume renderings with Holoscan
    - Adds Magic Leap scripts to simplify the developer interface to the
      `windrunner` backend
    - Updates custom container build process to separate "base" and
      "development" containers. Future work may support an additional
      "release" container.
    
    Adapts internal work to provide additional infrastructure and features
    for the `volume_rendering_xr` application. Additional support may
    follow.
    
    Many thanks to Magic Leap and NVIDIA collaborators. Contributors have
    been updated in the application metadata.
    
    co-authored-by: Cristiana Dinea <cdinea@nvidia.com>
    co-authored-by: Andreas Heumann <aheumann@nvidia.com>
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>

---
+++ applications/volume_rendering_xr/thirdparty/magicleap/ml_start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# check if the service is running
+if pgrep windrunner-serv >/dev/null; then
+    source ml_stop.sh
+fi
+
+if [ $# -eq 0 ]; then
+    XRT_SKIP_STDIN='true' windrunner-service &
+elif [ $1 == "debug" ]; then
+    XRT_SKIP_STDIN='true' XRT_DEBUG_GUI='true' XRT_LITE_UI='true'  windrunner-service &
+fi
+
+# check if the service is running
+if ! ps -p $! > /dev/null; then
+    echo "Failed to run windrunner service"
+    exit -1
+fi
+# wait for service startup
+until [ -e /tmp/windrunner_comp_ipc ]
+do
+     echo "Waiting for windrunner-service"
+     sleep 1
+done
