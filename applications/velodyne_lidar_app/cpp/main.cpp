2c9d08d52ddb74dfe0f5b5b3ae8a6ba89d8c904d
Tom Birdsong <tbirdsong@nvidia.com>
2024-04-23 16:47:26 -0400

    App: Demonstrate Lidar sensor streaming with HoloViz
    Adds an operator and application to demonstrate streaming from the
    Velodyne VLP-16 3D lidar sensor to display with HoloViz.
    
    Change:
    - Implements C++ `VelodyneLidarOp` for processing VLP-16 packets on GPU,
      informed by the NVIDIA DeepMap SDK
    - Implements C++ `VelodyneLidarApp` demonstration pipeline to receive
      VLP-16 packets over IP/UDP, process them into a Cartesian point cloud
      on GPU, and then pass them to HoloViz for display.
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>

---
+++ applications/velodyne_lidar_app/cpp/main.cpp
@@ -0,0 +1,52 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <iostream>
+
+#include <holoscan/holoscan.hpp>
+#include <holoscan/operators/holoviz/holoviz.hpp>
+
+#include <basic_network_operator_rx.h>
+#include <velodyne_lidar.hpp>
+
+class App : public holoscan::Application {
+ public:
+  void compose() override {
+    using namespace holoscan;
+
+    auto net_rx = make_operator<ops::BasicNetworkOpRx>(
+        "network_rx", from_config("network_rx"), make_condition<BooleanCondition>("is_alive"));
+    auto velodyne_op = make_operator<holoscan::ops::VelodyneLidarOp>("lidar", from_config("lidar"));
+    auto viz_op = make_operator<holoscan::ops::HolovizOp>("holoviz", from_config("holoviz"));
+
+    add_flow(net_rx, velodyne_op, {{"burst_out", "burst_in"}});
+    add_flow(velodyne_op, viz_op, {{"cloud_out", "receivers"}});
+  }
+};
+
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    std::cout << "Expected a YAML configuration file" << std::endl;
+    return 1;
+  }
+
+  holoscan::set_log_level(holoscan::LogLevel::INFO);
+  auto app = holoscan::make_application<App>();
+  app->config(argv[1]);
+  app->run();
+  return 0;
+}
---
+++ operators/velodyne_lidar/cpp/velodyne_convert_xyz.hpp
@@ -0,0 +1,103 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights
+ * reserved. SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Ported from NVIDIA DeepMap SDK
+
+#ifndef VELODYNE_CONVERT_XYZ_HPP
+#define VELODYNE_CONVERT_XYZ_HPP
+
+#include <cuda_runtime.h>
+#include <math.h>
+#include <stdint.h>
+#include <iostream>
+#include <vector>
+
+#include "velodyne_constants.hpp"
+
+namespace data_collection {
+namespace sensors {
+
+/// @brief Defines one point in the 3D coordinate system relative to the VLP-16 sensor.
+///
+/// Values are assumed to be lengths reported in meters.
+struct PointXYZ {
+  float x;
+  float y;
+  float z;
+};
+
+/// @brief Points corresponding to a single azimuth in a single VLP-16 packet.
+struct Lines {
+  PointXYZ lines_[kVelodyneRecords];
+};
+
+/// @brief Sets of points corresponding to all azimuths in a single VLP-16 packet.
+struct Blocks {
+  Lines blocks_[kVelodyneBlocks];
+};
+
+/// @brief Helper class to convert VLP-16 packets to a list of XYZ points in device memory.
+///
+/// This class is used to convert raw Velodyne packets to a list of XYZ points via GPU.
+/// It maintains an internal table of sin and cos values for yaw and pitch angles
+/// to reuse across packet transformations.
+/// Code is adapted from the NVIDIA Isaac DeepMap SDK.
+///
+/// @see https://developer.nvidia.com/isaac
+class VelodyneConvertXYZHelper {
+ public:
+  // Destructor of class. Mainly free the memory space.
+  ~VelodyneConvertXYZHelper();
+
+  /// @brief Convert a raw Velodyne VLP-16 packet to a list of XYZ points.
+  /// @param packet The 1206-byte packet in host memory to convert.
+  /// @param gpu_xyz_destination The device memory destination for 384 XYZ points.
+  ///
+  /// One Velodyne VLP-16 packet describes:
+  /// - 12 data blocks, each representing one azimuth angle;
+  /// - 2 firing sequences per data block;
+  /// - 16 spherical data points per firing sequence.
+  ///
+  /// This function copies a VLP-16 packet to device memory and
+  /// converts it to a list of XYZ points in device memory.
+  ///
+  /// This function does NOT:
+  /// - Encode additional information such as intensity;
+  /// - Discern the arc covered by the packet
+  void ConvertRawPacketToDeviceXYZ(const data_collection::sensors::RawVelodynePacket* packet,
+                                   PointXYZ* gpu_xyz_intensity_destination);
+
+ private:
+  // Initialize all yaw table and pitch table, and copy them into Gpu
+  // constants. This function will be called when first time call
+  // RawVelodynePacketToXYZIntensityGpu.
+  void InitSinAndCosTable();
+
+  // sin yaw. Cross 360 degrees. GPU data. resolution 0.01 degrees.
+  double* d_sin_rot_table_;
+  // cos yaw. Cross 360 degrees. GPU data. resolution 0.01 degrees.
+  double* d_cos_rot_table_;
+  // Raw Velodyne packets. GPU data.
+  RawVelodynePacket* d_packet_;
+  // If this flag is false. When calling RawVelodynePacketToXYZIntensityGpu will
+  // tries to init all tables.
+  bool initialized_ = false;
+};
+}  // namespace sensors
+}  // namespace data_collection
+
+#endif  // VELODYNE_CONVERT_XYZ_HPP
