c55eaa2db7d5c87d4b0bd89197f975cd00f99281
Victor Chang <mocsharp@users.noreply.github.com>
2024-08-05 06:18:32 -0700

    DevContainer for Holoscan SDK NGC Container (#425)
    * DevContainer for Holoscan SDK NGC Container
    
    Signed-off-by: Victor Chang <vicchang@nvidia.com>
    
    * Replace common-debian.sh with devcontainer feature
    
    - address review feedback
    - move under /tutorials/
    
    Signed-off-by: Victor Chang <vicchang@nvidia.com>
    
    * Update with HSDK 2.3 container with built-in debug symbols and source code
    
    - Replace fish with zsh
    - Reduce setup steps by removing the custom workspace step
    
    Signed-off-by: Victor Chang <vicchang@nvidia.com>
    
    * Clean up unused variables
    
    Signed-off-by: Victor Chang <vicchang@nvidia.com>
    
    * Minor fixes
    
    Signed-off-by: Victor Chang <vicchang@nvidia.com>
    
    * Describe the directories and how to change username, UID, GID
    - Make screenshots smaller
    
    Signed-off-by: Victor Chang <vicchang@nvidia.com>
    
    * Update overview section
    
    Signed-off-by: Victor Chang <vicchang@nvidia.com>
    
    * Rename directory to holoscan_container_vscode
    
    Signed-off-by: Victor Chang <vicchang@nvidia.com>
    
    * Fix launch task name for examples/hello_world/python
    
    Signed-off-by: Victor Chang <vicchang@nvidia.com>
    
    ---------
    
    Signed-off-by: Victor Chang <vicchang@nvidia.com>
    Signed-off-by: Rony Rado <rrado@nvidia.com>

---
+++ tutorials/holoscan_container_vscode/.devcontainer/post-start-command.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+printf "#################### .devcontainer/post-start-command.sh ####################\n"
+printf "#                                                                           #\n"
+printf "# Edit this file to add commands that should be run after the container is  #\n"
+printf "# started.                                                                  #\n"
+printf "#                                                                           #\n"
+printf "#############################################################################\n"
+
+# Create directories and set permissions for the holoscan user
+sudo mkdir -p /home/holoscan/
+
+echo Copying Holoscan SDK example applications to ~/examples...
+sudo cp -r /opt/nvidia/holoscan/examples /home/holoscan/examples
+
+echo Configuring /home/holoscan for $USER...
+sudo chown -R $USER:$USER /home/holoscan
+
+# Set shell welcome message
+if ! grep -q "Welcome" ~/.zshrc 2>/dev/null ; then
+   echo "echo \"Welcome to Holoscan SDK NGC container!\n\n\
+Here's a list of useful locations:\n\n\
+       - Workspace: /workspace \n\
+       - Holoscan SDK Installation: /opt/nvidia/holoscan \n\
+       - Holoscan SDK Source Code: /workspace/holoscan-sdk \n\
+       - Sample applications: /workspace/holoscan-sdk/examples\n\n\
+       - Sample data: /opt/nvidia/holoscan/data\n\n\
+Checkout the /workspace/my/README.md file for more information on the Holoscan SDK DevContainer.\n\n\"" >> ~/.zshrc
+fi
