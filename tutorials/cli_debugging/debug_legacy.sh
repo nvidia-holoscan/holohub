ec7ca4bbc926f31b9735d08a39ae680ab4c4a1ad
Tom Birdsong <40648863+tbirdso@users.noreply.github.com>
2024-08-05 09:41:34 -0400

    Tutorial: Add tutorial on just-in-time Holoscan debugging (#433)
    * Tutorial: Add tutorial on just-in-time Holoscan debugging
    
    Adds a tutorial to walk through setting up just-in-time debugging with
    GDB or `pdb` debugging tools for a Holoscan SDK application using
    HoloHub infrastructure.
    
    Includes scripts for runnable examples illustrating just-in-time
    debugging. Streamlined instructions for v2.3 rely on debugging symbols
    to be distributed in the Holoscan SDK NGC development container, while
    general instructions for <=v2.2 are also included.
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Add legacy build patch
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Corrections for Python debugging with GDB/PDB
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Fix: Check in v2.2 Vulkan patch for tutorial
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Apply review comments
    
    co-authored-by: Victor Chang <vicchang@nvidia.com>
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Rename to `cli_debugging`
    
    Rename to differentiate from IDE-based debugging (VSCode)
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Address review comments
    
    co-authored-by: Zhijin Li <zhijinl@nvidia.com>
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Address review feedback
    
    Co-authored-by: Maximillian Ofir <maxofir@nvidia.com>
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    ---------
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    Co-authored-by: Victor Chang <vicchang@nvidia.com>
    Co-authored-by: Zhijin Li <zhijinl@nvidia.com>
    Co-authored-by: Maximillian Ofir <maxofir@nvidia.com>
    Signed-off-by: Rony Rado <rrado@nvidia.com>

---
+++ tutorials/cli_debugging/debug_legacy.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -e
+
+# This script builds Holoscan SDK with debug symbols and runs the Endoscopy Tool Tracking application with GDB.
+# Usage: ./debug_legacy.sh [build_type:debug,rel-debug,release]
+
+build_type=${1:-rel-debug}
+holoscan_sdk_tag="v2.2.0"
+holoscan_image="nvcr.io/nvidia/clara-holoscan/holoscan:v2.2.0-dgpu"
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+HOLOHUB_ROOT=$(realpath "${SCRIPT_DIR}/../..")
+tmp_dir=${SCRIPT_DIR}/tmp/cli_debugging
+mkdir -p ${tmp_dir}
+
+# Download and build Holoscan SDK
+pushd ${tmp_dir}
+if [ ! -d holoscan-sdk ]; then
+    git clone git@github.com:nvidia-holoscan/holoscan-sdk.git
+fi
+cd holoscan-sdk
+git checkout ${holoscan_sdk_tag}
+# Apply patch to fix broken Vulkan SDK source
+# https://github.com/nvidia-holoscan/holoscan-sdk/issues/30
+git reset --hard HEAD
+git apply ${SCRIPT_DIR}/holoscan_sdk_20240723_1.diff
+./run build --type $build_type
+INSTALL_DIR=$(realpath $(find . -type d -name "install-*"))
+popd
+
+# Build the tutorial container with GDB
+${HOLOHUB_ROOT}/dev_container build \
+    --img holohub:debugging \
+    --docker_file ${SCRIPT_DIR}/Dockerfile \
+    --base_img ${holoscan_image}
+
+# Build the Endoscopy Tool Tracking application with debugging symbols
+${HOLOHUB_ROOT}/dev_container launch \
+    --docker_opts "-v ${INSTALL_DIR}:/opt/nvidia/holoscan" \
+    --img holohub:debugging \
+    -- bash -c "./run build endoscopy_tool_tracking --type ${build_type}"
+
+# Launch GDB with the Endoscopy Tool Tracking application
+${HOLOHUB_ROOT}/dev_container launch \
+    --docker_opts "-v ${INSTALL_DIR}:/opt/nvidia/holoscan --security-opt seccomp=unconfined" \
+    --img holohub:debugging \
+    --  bash -c \
+            'cd /workspace/holohub/build/endoscopy_tool_tracking && \
+            gdb -q \
+                -ex "break main" \
+                -ex "run --data /workspace/holohub/data/endoscopy" \
+                -ex "break /workspace/holoscan-sdk/src/core/application.cpp:add_flow" \
+                /workspace/holohub/build/endoscopy_tool_tracking/applications/endoscopy_tool_tracking/cpp/endoscopy_tool_tracking'
