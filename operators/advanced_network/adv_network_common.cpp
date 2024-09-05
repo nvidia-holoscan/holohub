3db3074915ba29c6d12f512dc65bc65f85249171
Rony Rado <rrado@nvidia.com>
2024-09-06 00:24:32 +0300

    Use a macro for repeatable null check of g_ano_mgr
    Using a macro for the repeated null check and assertion of g_ano_mgr to improve readability and maintainability,
    
    Signed-off-by: Rony Rado <rrado@nvidia.com>

--- operators/advanced_network/adv_network_common.cpp
+++ operators/advanced_network/adv_network_common.cpp
@@ -22,6 +22,7 @@
 #include <rte_memcpy.h>
 #include <rte_ethdev.h>
 
+#define ASSERT_ANO_MGR_INITIALIZED() assert(g_ano_mgr != nullptr && "ANO Manager is not initialized")
 namespace holoscan::ops {
 
 /**
@@ -37,6 +38,7 @@ static ANOMgr* g_ano_mgr = nullptr;
 
 
 AdvNetBurstParams *adv_net_create_burst_params() {
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->create_burst_params();
 }
 
@@ -61,17 +63,17 @@ AnoMgrType adv_net_get_manager_type(const Config& config) {
 template AnoMgrType adv_net_get_manager_type<Config>(const Config&);
 
 void adv_net_free_pkt(AdvNetBurstParams* burst, int pkt) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   g_ano_mgr->free_pkt(burst, pkt);
 }
 
 void adv_net_free_pkt(std::shared_ptr<AdvNetBurstParams> burst, int pkt) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   adv_net_free_pkt(burst.get(), pkt);
 }
 
 void adv_net_free_pkt_seg(AdvNetBurstParams* burst, int seg, int pkt) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   g_ano_mgr->free_pkt_seg(burst, seg, pkt);
 }
 
@@ -80,22 +82,22 @@ void adv_net_free_pkt_seg(std::shared_ptr<AdvNetBurstParams> burst, int seg, int
 }
 
 uint16_t adv_net_get_pkt_len(AdvNetBurstParams* burst, int idx) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->get_pkt_len(burst, idx);
 }
 
 uint64_t adv_net_get_burst_tot_byte(std::shared_ptr<AdvNetBurstParams> burst) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->get_burst_tot_byte(burst.get());
 }
 
 uint16_t adv_net_get_pkt_len(std::shared_ptr<AdvNetBurstParams> burst, int idx) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return adv_net_get_pkt_len(burst.get(), idx);
 }
 
 uint16_t adv_net_get_seg_pkt_len(AdvNetBurstParams* burst, int seg, int idx) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->get_seg_pkt_len(burst, seg, idx);
 }
 
@@ -104,7 +106,7 @@ uint16_t adv_net_get_seg_pkt_len(std::shared_ptr<AdvNetBurstParams> burst, int s
 }
 
 void adv_net_free_all_seg_pkts(AdvNetBurstParams* burst, int seg) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   g_ano_mgr->free_all_seg_pkts(burst, seg);
 }
 
@@ -113,7 +115,7 @@ void adv_net_free_all_seg_pkts(std::shared_ptr<AdvNetBurstParams> burst, int seg
 }
 
 void adv_net_free_all_burst_pkts(AdvNetBurstParams* burst) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   g_ano_mgr->free_all_pkts(burst);
 }
 
@@ -123,7 +125,7 @@ void adv_net_free_all_burst_pkts(std::shared_ptr<AdvNetBurstParams> burst) {
 
 void adv_net_free_all_pkts_and_burst(AdvNetBurstParams* burst) {
   adv_net_free_all_burst_pkts(burst);
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   g_ano_mgr->free_rx_burst(burst);
 }
 
@@ -132,7 +134,7 @@ void adv_net_free_all_pkts_and_burst(std::shared_ptr<AdvNetBurstParams> burst) {
 }
 
 void adv_net_free_seg_pkts_and_burst(AdvNetBurstParams* burst, int seg) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   g_ano_mgr->free_all_seg_pkts(burst, seg);
   g_ano_mgr->free_rx_burst(burst);
 }
@@ -158,12 +160,12 @@ void adv_net_format_eth_addr(char* dst, std::string addr) {
 }
 
 AdvNetStatus adv_net_get_mac(int port, char* mac) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->get_mac(port, mac);
 }
 
 bool adv_net_tx_burst_available(AdvNetBurstParams* burst) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->tx_burst_available(burst);
 }
 
@@ -172,12 +174,12 @@ bool adv_net_tx_burst_available(std::shared_ptr<AdvNetBurstParams> burst) {
 }
 
 int adv_net_address_to_port(const std::string& addr) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->address_to_port(addr);
 }
 
 AdvNetStatus adv_net_get_tx_pkt_burst(AdvNetBurstParams* burst) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   if (!g_ano_mgr->tx_burst_available(burst)) return AdvNetStatus::NO_FREE_BURST_BUFFERS;
   return g_ano_mgr->get_tx_pkt_burst(burst);
 }
@@ -187,7 +189,7 @@ AdvNetStatus adv_net_get_tx_pkt_burst(std::shared_ptr<AdvNetBurstParams> burst) 
 }
 
 AdvNetStatus adv_net_set_eth_hdr(AdvNetBurstParams* burst, int idx, char* dst_addr) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->set_eth_hdr(burst, idx, dst_addr);
 }
 
@@ -198,7 +200,7 @@ AdvNetStatus adv_net_set_eth_hdr(std::shared_ptr<AdvNetBurstParams> burst, int i
 
 AdvNetStatus adv_net_set_ipv4_hdr(AdvNetBurstParams* burst, int idx, int ip_len, uint8_t proto,
                                   unsigned int src_host, unsigned int dst_host) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->set_ipv4_hdr(burst, idx, ip_len, proto, src_host, dst_host);
 }
 
@@ -209,7 +211,7 @@ AdvNetStatus adv_net_set_ipv4_hdr(std::shared_ptr<AdvNetBurstParams> burst, int 
 
 AdvNetStatus adv_net_set_udp_hdr(AdvNetBurstParams* burst, int idx, int udp_len, uint16_t src_port,
                                  uint16_t dst_port) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->set_udp_hdr(burst, idx, udp_len, src_port, dst_port);
 }
 
@@ -219,7 +221,7 @@ AdvNetStatus adv_net_set_udp_hdr(std::shared_ptr<AdvNetBurstParams> burst, int i
 }
 
 AdvNetStatus adv_net_set_udp_payload(AdvNetBurstParams* burst, int idx, void* data, int len) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->set_udp_payload(burst, idx, data, len);
 }
 
@@ -230,7 +232,7 @@ AdvNetStatus adv_net_set_udp_payload(std::shared_ptr<AdvNetBurstParams> burst, i
 
 AdvNetStatus adv_net_set_pkt_lens(AdvNetBurstParams* burst, int idx,
                                   const std::initializer_list<int>& lens) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->set_pkt_lens(burst, idx, lens);
 }
 
@@ -240,7 +242,7 @@ AdvNetStatus adv_net_set_pkt_lens(std::shared_ptr<AdvNetBurstParams> burst, int 
 }
 
 AdvNetStatus adv_net_set_pkt_tx_time(AdvNetBurstParams* burst, int idx, uint64_t time) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->set_pkt_tx_time(burst, idx, time);
 }
 
@@ -289,7 +291,7 @@ void adv_net_set_hdr(std::shared_ptr<AdvNetBurstParams> burst, uint16_t port, ui
 }
 
 void adv_net_free_tx_burst(AdvNetBurstParams* burst) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   g_ano_mgr->free_tx_burst(burst);
 }
 
@@ -298,7 +300,7 @@ void adv_net_free_tx_burst(std::shared_ptr<AdvNetBurstParams> burst) {
 }
 
 void adv_net_free_rx_burst(AdvNetBurstParams* burst) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   g_ano_mgr->free_rx_burst(burst);
 }
 
@@ -307,7 +309,7 @@ void adv_net_free_rx_burst(std::shared_ptr<AdvNetBurstParams> burst) {
 }
 
 void* adv_net_get_seg_pkt_ptr(AdvNetBurstParams* burst, int seg, int idx) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->get_seg_pkt_ptr(burst, seg, idx);
 }
 
@@ -316,7 +318,7 @@ void* adv_net_get_seg_pkt_ptr(std::shared_ptr<AdvNetBurstParams> burst, int seg,
 }
 
 void* adv_net_get_pkt_ptr(AdvNetBurstParams* burst, int idx) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->get_pkt_ptr(burst, idx);
 }
 
@@ -325,17 +327,17 @@ void* adv_net_get_pkt_ptr(std::shared_ptr<AdvNetBurstParams> burst, int idx) {
 }
 
 std::optional<uint16_t> adv_net_get_port_from_ifname(const std::string& name) {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   return g_ano_mgr->get_port_from_ifname(name);
 }
 
 void adv_net_shutdown() {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   g_ano_mgr->shutdown();
 }
 
 void adv_net_print_stats() {
-  assert(g_ano_mgr != nullptr && "ANO Manager is not initialized");
+  ASSERT_ANO_MGR_INITIALIZED();
   g_ano_mgr->print_stats();
 }
 
