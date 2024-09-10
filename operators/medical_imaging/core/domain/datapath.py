b916fd5cb3eb0eee8168d2678953e213ba7539a9
Ming M Qin <38891913+MMelQin@users.noreply.github.com>
2024-09-10 11:59:46 -0700

    Introducing medical imaging post processing operators and applications to Holohub (#423)
    * Add medical imaging operators and example application
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Applied Holohub lintting rules
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Fix "ruff check" complaints
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Fix more "ruff check" complaints
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * More "lint --fix"
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Add metadata for operators
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * "lint --fix" application
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * "code spell" complaint fix
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Fix "codespell" complaints
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Last fix
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Fine tune requirements files
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Add README and script file for setting app env vars
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Make import consistent
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Import consistency
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Linting fix
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Correctly locating model file under the given path
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Minor doc update
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * CMakeList.txt added for app to work with build_and_run, and other changes
    
    Change to depend on HSDK 2.3
    CLI paclager shell script added, needs testing
    README.md still needs to be updated
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Updated README, packaging, docker file, ready for formal review
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Correct typo
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Clean up comments and fix a couple typos
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * finalized  the license and added Notices in the new README
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Added arm64 support to metadata
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    * Correct a typo and add a comment on commented commands.
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>
    
    ---------
    
    Signed-off-by: M Q <mingmelvinq@nvidia.com>

---
+++ operators/medical_imaging/core/domain/datapath.py
@@ -0,0 +1,84 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
+from typing import Dict, Optional, Union
+
+from operators.medical_imaging.exceptions import IOMappingError as IOMappingError
+from operators.medical_imaging.exceptions import ItemNotExistsError as ItemNotExistsError
+
+from .domain import Domain as Domain
+
+
+class DataPath(Domain):
+    def __init__(
+        self, path: Union[str, Path], read_only: bool = False, metadata: Optional[Dict] = None
+    ):
+        """Initializes a DataPath object.
+
+        Args:
+            path (Union[str, Path]): Path to the data file/directory.
+            read_only (bool): True if the the file/directory path cannot be modified.
+            metadata (Optional[Dict]): A metadata.
+        """
+        super().__init__(metadata=metadata)
+        self._path: Path = Path(path)
+        self._read_only: bool = read_only
+
+    @property
+    def path(self):
+        """Returns the path of the data file/directory."""
+        return self._path
+
+    @path.setter
+    def path(self, val):
+        if self._read_only:
+            raise IOMappingError("This DataPath is read-only.")
+        self._path = Path(val)
+
+    def to_absolute(self):
+        """Convert the internal representation of the path to an absolute path."""
+        if not self._path.is_absolute():
+            self._path = self._path.absolute()
+
+
+class NamedDataPath(Domain):
+    """A data path dictionary with name as key and data path as value.
+
+    This class is used to store data paths and the provided name of each data path is unique.
+
+    A data path for a name is accessible by calling the `get()` method with the name.
+
+    If only one data path is available and the name is not specified, the data path is returned.
+    """
+
+    def __init__(self, paths: Dict[str, DataPath], metadata: Optional[Dict] = None):
+        super().__init__(metadata=metadata)
+        self._paths = paths
+
+    def get(self, name: Optional[str] = "") -> DataPath:
+        if name not in self._paths:
+            if name == "" and len(self._paths) == 1:
+                return next(iter(self._paths.values()))
+            else:
+                raise IOMappingError(
+                    f"{name!r} is not a valid name. It should be one of ({', '.join(self._paths.keys())})."
+                )
+        else:
+            datapath = self._paths.get(name)
+            if not datapath:
+                raise ItemNotExistsError(f"A DataPath instance for {name!r} does not exist.")
+            return datapath
