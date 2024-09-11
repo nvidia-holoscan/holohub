61c50bd27ee224db3965f1c97ad9ad4dab255fae
Rony Rado <rrado@nvidia.com>
2024-09-11 23:14:56 +0300

    Add split_boundary to RxQueueConfig YAML parsing
    Updated parse_rx_queue_common_config to include split_boundary field from YAML configuration.
    
    Set q.common_.split_boundary_ = q_item["split_boundary"].as<int>(0), with a default value of 0.
    
    Signed-off-by: Rony Rado <rrado@nvidia.com>

--- operators/advanced_network/adv_network_common.cpp
+++ operators/advanced_network/adv_network_common.cpp
@@ -406,6 +406,7 @@ bool YAML::convert<holoscan::ops::AdvNetConfigYaml>::parse_rx_queue_common_confi
     q.common_.id_ = q_item["id"].as<int>();
     q.common_.cpu_core_ = q_item["cpu_core"].as<std::string>();
     q.common_.batch_size_ = q_item["batch_size"].as<int>();
+    q.common_.split_boundary_ = q_item["split_boundary"].as<int>(0);
     q.common_.extra_queue_config = nullptr;
     q.output_port_ = q_item["output_port"].as<std::string>();
 
