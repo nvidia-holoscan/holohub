cdfcd830e49fb6ef6af41984282e892ff07306a2
Andreas Heumann <aheumann@nvidia.com>
2024-10-09 14:38:28 +0200

    Update multiai ultrasound application for optimal performance (#522)
    Use CUDA stream pool to enable operators to run in parallel on the GPU.
    Use BlockMemoryPool to avoid calls to cudaFree() which synchronize
    CPU and GPU execution.
    Execute post processing on GPU (needs Holoscan SDK 2.6).
    
    Improves performance from 12 ms to 7.4 ms per frame.
    
    Signed-off-by: Andreas Heumann <aheumann@nvidia.com>

--- applications/multiai_ultrasound/cpp/main.cpp
+++ applications/multiai_ultrasound/cpp/main.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2022-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2022-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,14 +17,19 @@
 
 #include <getopt.h>
 
-#include "holoscan/holoscan.hpp"
+#include <holoscan/holoscan.hpp>
 #include <holoscan/operators/aja_source/aja_source.hpp>
-#include <holoscan/operators/video_stream_replayer/video_stream_replayer.hpp>
 #include <holoscan/operators/format_converter/format_converter.hpp>
+#include <holoscan/operators/holoviz/holoviz.hpp>
 #include <holoscan/operators/inference/inference.hpp>
 #include <holoscan/operators/inference_processor/inference_processor.hpp>
+#include <holoscan/operators/video_stream_replayer/video_stream_replayer.hpp>
+#include <holoscan/version_config.hpp>
+
 #include <visualizer_icardio.hpp>
-#include <holoscan/operators/holoviz/holoviz.hpp>
+
+#define HOLOSCAN_VERSION \
+  (HOLOSCAN_VERSION_MAJOR * 10000 + HOLOSCAN_VERSION_MINOR * 100 + HOLOSCAN_VERSION_PATCH)
 
 class App : public holoscan::Application {
  public:
@@ -32,82 +37,121 @@ class App : public holoscan::Application {
     if (source == "aja") { is_aja_source_ = true; }
   }
 
-  void set_datapath(const std::string& path) {
-     datapath = path;
-  }
+  void set_datapath(const std::string& path) { datapath = path; }
 
   void compose() override {
     using namespace holoscan;
 
     std::shared_ptr<Operator> source;
-    std::shared_ptr<Resource> pool_resource = make_resource<UnboundedAllocator>("pool");
+    const std::shared_ptr<CudaStreamPool> cuda_stream_pool =
+        make_resource<CudaStreamPool>("cuda_stream");
+
     if (is_aja_source_) {
       source = make_operator<ops::AJASourceOp>("aja", from_config("aja"));
     } else {
-      source = make_operator<ops::VideoStreamReplayerOp>("replayer", from_config("replayer"),
-                                                                     Arg("directory", datapath));
+      source = make_operator<ops::VideoStreamReplayerOp>(
+          "replayer", from_config("replayer"), Arg("directory", datapath));
     }
 
     auto in_dtype = is_aja_source_ ? std::string("rgba8888") : std::string("rgb888");
-    auto plax_cham_resized = make_operator<ops::FormatConverterOp>("plax_cham_resized",
-                                                                   from_config("plax_cham_resized"),
-                                                                   Arg("in_dtype") = in_dtype,
-                                                                   Arg("pool") = pool_resource);
+    auto in_components = is_aja_source_ ? 4 : 3;
+    auto plax_cham_pre =
+        make_operator<ops::FormatConverterOp>("plax_cham_pre",
+                                              from_config("plax_cham_pre"),
+                                              Arg("in_dtype") = in_dtype,
+                                              Arg("pool") = make_resource<BlockMemoryPool>(
+                                                  "plax_cham_pre_pool",
+                                                  (int32_t)nvidia::gxf::MemoryStorageType::kDevice,
+                                                  320 * 320 * sizeof(float) * in_components,
+                                                  3),
+                                              Arg("cuda_stream_pool") = cuda_stream_pool);
 
-    auto plax_cham_pre = make_operator<ops::FormatConverterOp>("plax_cham_pre",
-                                                               from_config("plax_cham_pre"),
-                                                               Arg("in_dtype") = in_dtype,
-                                                               Arg("pool") = pool_resource);
+    auto aortic_ste_pre =
+        make_operator<ops::FormatConverterOp>("aortic_ste_pre",
+                                              from_config("aortic_ste_pre"),
+                                              Arg("in_dtype") = in_dtype,
+                                              Arg("pool") = make_resource<BlockMemoryPool>(
+                                                  "aortic_ste_pre_pool",
+                                                  (int32_t)nvidia::gxf::MemoryStorageType::kDevice,
+                                                  300 * 300 * sizeof(float) * in_components,
+                                                  3),
+                                              Arg("cuda_stream_pool") = cuda_stream_pool);
 
-    auto aortic_ste_pre = make_operator<ops::FormatConverterOp>("aortic_ste_pre",
-                                                                from_config("aortic_ste_pre"),
-                                                                Arg("in_dtype") = in_dtype,
-                                                                Arg("pool") = pool_resource);
-
-    auto b_mode_pers_pre = make_operator<ops::FormatConverterOp>("b_mode_pers_pre",
-                                                                 from_config("b_mode_pers_pre"),
-                                                                 Arg("in_dtype") = in_dtype,
-                                                                 Arg("pool") = pool_resource);
+    auto b_mode_pers_pre =
+        make_operator<ops::FormatConverterOp>("b_mode_pers_pre",
+                                              from_config("b_mode_pers_pre"),
+                                              Arg("in_dtype") = in_dtype,
+                                              Arg("pool") = make_resource<BlockMemoryPool>(
+                                                  "b_mode_pers_pre_pool",
+                                                  (int32_t)nvidia::gxf::MemoryStorageType::kDevice,
+                                                  320 * 240 * sizeof(float) * in_components,
+                                                  3),
+                                              Arg("cuda_stream_pool") = cuda_stream_pool);
 
     ops::InferenceOp::DataMap model_path_map;
     model_path_map.insert("plax_chamber", datapath + "/plax_chamber.onnx");
     model_path_map.insert("aortic_stenosis", datapath + "/aortic_stenosis.onnx");
     model_path_map.insert("bmode_perspective", datapath + "/bmode_perspective.onnx");
 
-    auto multiai_inference = make_operator<ops::InferenceOp>(
-        "multiai_inference", from_config("multiai_inference"),
-        Arg("model_path_map", model_path_map),
-        Arg("allocator") = pool_resource);
+    const auto plax_chamber_output_size = 320 * 320 * sizeof(float) * 6;
+    const auto aortic_stenosis_output_size = sizeof(float) * 2;
+    const auto bmode_perspective_output_size = sizeof(float) * 1;
+    auto block_size =
+        std::max(plax_chamber_output_size,
+                 std::max(aortic_stenosis_output_size, bmode_perspective_output_size));
+    auto multiai_inference =
+        make_operator<ops::InferenceOp>("multiai_inference",
+                                        from_config("multiai_inference"),
+                                        Arg("model_path_map", model_path_map),
+                                        Arg("allocator") = make_resource<BlockMemoryPool>(
+                                            "multiai_inference_allocator",
+                                            (int32_t)nvidia::gxf::MemoryStorageType::kDevice,
+                                            block_size,
+                                            2 * 3),
+                                        Arg("cuda_stream_pool") = cuda_stream_pool);
 
-    auto multiai_postprocessor =
-        make_operator<ops::InferenceProcessorOp>("multiai_postprocessor",
-                                                   from_config("multiai_postprocessor"),
-                                                   Arg("allocator") = pool_resource);
+    //  version 2.6 supports the CUDA version of `max_per_channel_scaled`
+    const bool supports_cuda_processing =
+#if HOLOSCAN_VERSION >= 20600
+        true;
+#else
+        false;
+#endif
+    auto multiai_postprocessor = make_operator<ops::InferenceProcessorOp>(
+        "multiai_postprocessor",
+        from_config("multiai_postprocessor"),
+        Arg("input_on_cuda", supports_cuda_processing),
+        Arg("output_on_cuda", supports_cuda_processing),
+        Arg("allocator") =
+            make_resource<BlockMemoryPool>("multiai_postprocessor_allocator",
+                                           (int32_t)nvidia::gxf::MemoryStorageType::kDevice,
+                                           // 2 float coordinates, 6 categories
+                                           2 * sizeof(float) * 6,
+                                           1),
+        Arg("cuda_stream_pool") = cuda_stream_pool);
 
     auto visualizer_icardio = make_operator<ops::VisualizerICardioOp>(
-        "visualizer_icardio", from_config("visualizer_icardio"),
+        "visualizer_icardio",
+        from_config("visualizer_icardio"),
         Arg("data_dir") = datapath,
-        Arg("allocator") = pool_resource);
+        Arg("allocator") =
+            make_resource<BlockMemoryPool>("visualizer_icardio_allocator",
+                                           (int32_t)nvidia::gxf::MemoryStorageType::kDevice,
+                                           // max from VisualizerICardioOp::tensor_to_shape_
+                                           320 * 320 * 4 * sizeof(uint8_t),
+                                           1 * 8),
+        Arg("cuda_stream_pool") = cuda_stream_pool);
 
     auto holoviz = make_operator<ops::HolovizOp>(
-        "holoviz", from_config("holoviz"), Arg("allocator") = pool_resource);
+        "holoviz", from_config("holoviz"), Arg("cuda_stream_pool") = cuda_stream_pool);
 
     // Flow definition
 
-    if (is_aja_source_) {
-      const std::set<std::pair<std::string, std::string>> aja_ports = {{"video_buffer_output", ""}};
-      add_flow(source, plax_cham_resized, aja_ports);
-      add_flow(source, plax_cham_pre, aja_ports);
-      add_flow(source, aortic_ste_pre, aja_ports);
-      add_flow(source, b_mode_pers_pre, aja_ports);
-    } else {
-      add_flow(source, plax_cham_resized);
-      add_flow(source, plax_cham_pre);
-      add_flow(source, aortic_ste_pre);
-      add_flow(source, b_mode_pers_pre);
-    }
-
-    add_flow(plax_cham_resized, holoviz, {{"", "receivers"}});
+    const std::string source_port_name = is_aja_source_ ? "video_buffer_output" : "";
+    add_flow(source, plax_cham_pre, {{source_port_name, ""}});
+    add_flow(source, aortic_ste_pre, {{source_port_name, ""}});
+    add_flow(source, b_mode_pers_pre, {{source_port_name, ""}});
+    add_flow(source, holoviz, {{source_port_name, "receivers"}});
 
     add_flow(plax_cham_pre, multiai_inference, {{"", "receivers"}});
     add_flow(aortic_ste_pre, multiai_inference, {{"", "receivers"}});
@@ -133,13 +177,9 @@ class App : public holoscan::Application {
 
 /** Helper function to parse the command line arguments */
 bool parse_arguments(int argc, char** argv, std::string& config_name, std::string& data_path) {
-  static struct option long_options[] = {
-      {"data",    required_argument, 0,  'd' },
-      {0,         0,                 0,  0 }
-  };
+  static struct option long_options[] = {{"data", required_argument, 0, 'd'}, {0, 0, 0, 0}};
 
-  while (int c = getopt_long(argc, argv, "d",
-                   long_options, NULL))  {
+  while (int c = getopt_long(argc, argv, "d", long_options, NULL)) {
     if (c == -1 || c == '?') break;
 
     switch (c) {
@@ -152,9 +192,7 @@ bool parse_arguments(int argc, char** argv, std::string& config_name, std::strin
     }
   }
 
-  if (optind < argc) {
-    config_name = argv[optind++];
-  }
+  if (optind < argc) { config_name = argv[optind++]; }
   return true;
 }
 
@@ -164,9 +202,7 @@ int main(int argc, char** argv) {
   // Parse the arguments
   std::string data_path = "";
   std::string config_name = "";
-  if (!parse_arguments(argc, argv, config_name, data_path)) {
-    return 1;
-  }
+  if (!parse_arguments(argc, argv, config_name, data_path)) { return 1; }
 
   if (config_name != "") {
     app->config(config_name);
