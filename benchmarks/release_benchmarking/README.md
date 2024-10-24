446803cfe32c0e9bd9e291c3cf48daf6fc9c58e2
Tom Birdsong <40648863+tbirdso@users.noreply.github.com>
2024-10-24 12:02:06 -0400

    Tutorial: Reproducible Workflow for Release Benchmarking (#472)
    * Benchmark: Update flow tracking chart generation script
    
    Updates to support using the graph generation script in other workflows:
    - Use `argparse` and prefer optional flags over optional positional
      arguments to minimize default input requirements
    - Add CLI args to update graph presentation settings
    - Generalize output filename for app input
    - Allow disabling CTest output
    
    Motivated by subsequent work on reproducible release benchmarking.
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Benchmark: Add Release Benchmarking tutorial
    
    Adds a benchmarking tutorial workflow to collect and present
    benchmarking statistics for a Holoscan SDK release.
    
    The tutorial aims to define a reproducible workflow based on existing
    Holoscan SDK and HoloHub tools that can be run with each Holoscan SDK
    release. Includes container with reporting tools and instructions for
    how to reproduce each component of the workflow.
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * App: Add `headless` entry in YAML configurations
    
    Add a `headless: false` entry in Endoscopy Tool Tracking and Multi-AI
    Ultrasound configuration files.
    
    This addition results in no change in default behavior, but makes it
    easier to automatically update YAML files for headless mode with a
    simple `sed` replacement command.
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Benchmark: Update log naming convention for clarity
    
    Updates output log filename convention in the Release Benchmarking
    tutorial for clarity. By convention we note parameter values in the log
    output filename as a safeguard for later reference.
    
    Previous example: `endoscopy_tool_tracking_3_1_1000_greedy_false_false`
    Updated example:
    `endoscopy_tool_tracking_3_1_1000_greedy_headless_offline`
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Address review feedback
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Benchmark: Minor updates for Holoscan Flow Benchmarking scripts
    
    Changes:
    - Document latency CSV statistics file assumptions in
      `generate_bar_graph.py`
    - Add debug output
    - Fix typos
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Benchmark: Fixes for `release_benchmarking` tutorial
    
    Changes:
    - Install and update `matplotlib` rendering dependencies for SVG
      processing
    - Fix latency results groups order to conform to underlying assumptions
      in `generate_bar_graph.py`
    - Fix log file path expansion to address issue where files were
      sometimes not found for processing on first run
    - Update README with troubleshooting
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Benchmark: Add v2.3.0 HoloHub benchmark report
    
    Add v2.3.0 benchmark report in markdown format
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Benchmark: Update Release Benchmarking tutorial README
    
    - Link to v2.3.0 report
    - Update description with feedback
    - Add developer references section
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Soham Sinha <soham.m17@gmail.com>
    Signed-off-by: Tom Birdsong <40648863+tbirdso@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>
    Signed-off-by: Tom Birdsong <40648863+tbirdso@users.noreply.github.com>
    Co-authored-by: Soham Sinha <soham.m17@gmail.com>

---
+++ benchmarks/release_benchmarking/README.md
@@ -0,0 +1,191 @@
+# Release Benchmarking Guide
+
+This tutorial provides a reproducible workflow for developers to accurately measure the latency of curated HoloHub
+applications across various SDK releases and different deployment scenarios, from single-application
+to multi-model use cases.
+
+Developers can use the [Holoscan Flow Benchmarking](../holoscan_flow_benchmarking/) tools referenced within this guide to systematically
+analyze performance bottlenecks, optimize execution times, and fine-tune their own applications for
+real-time, low-latency processing.
+
+## Contents
+
+- [Background](#background)
+- [Previous Holoscan Release Benchmark Reports](#previous-release-reports)
+- Running the Tutorial
+  - [Running Benchmarks](#running-benchmarks-getting-started)
+  - [Summarizing Data](#summarizing-data)
+  - [Presenting Data](#presenting-data)
+- [Troubleshooting](#troubleshooting)
+- [Developer References](#developer-references)
+
+## Background
+
+Holoscan SDK emphasizes low end-to-end latency in application pipelines. In addition to other
+benchmarks, we can use HoloHub applications to evaluate Holoscan SDK performance over releases.
+
+In this tutorial we provide a reproducible workflow to evaluate end-to-end latency performance on
+the [Endoscopy Tool Tracking](/applications/endoscopy_tool_tracking/) and
+[Multi-AI Ultrasound](/applications/multiai_ultrasound) HoloHub projects. These projects are generally
+maintained by the NVIDIA Holoscan team and demonstrate baseline Holoscan SDK inference pipelines
+with video replay and Holoviz rendering output.
+
+Benchmark scenarios include:
+- Running multiple Holoscan SDK pipelines concurrently on a single machine
+- Running video replay input at real-time speeds or as fast as possible
+- Running Holoviz output with either visual rendering or in headless mode
+
+We plan to release HoloHub benchmarks in the [release subfolder](release) following Holoscan SDK general
+releases. You can follow the tutorial below to similarly evaluate performance on your own machine.
+
+Refer to related documents for more information:
+- the [results report template file](template/results.md.tmpl) provides additional information on
+definitions and background
+- versioned releases are available for review in the [release subfolder](release)
+
+## Previous Holoscan Release Benchmark Reports
+
+- [Holoscan SDK v2.3.0](./release/v2.3.0/v2.3.0.md)
+
+## Running Benchmarks: Getting Started
+
+Data collection can be run in the HoloHub base container for both the Endoscopy Tool Tracking and the Multi-AI Ultrasound applications. We've provided a custom Dockerfile with tools to process collected data into a benchmark report.
+
+```bash
+# Build the container
+./dev_container build \
+    --img holohub:release_benchmarking \
+    --docker_file benchmarks/release_benchmarking/Dockerfile \
+    --base_img nvcr.io/nvidia/clara-holoscan/holoscan:<holoscan-sdk-version>-$(./dev_container get_host_gpu)
+
+# Launch the dev environment
+./dev_container launch --img holohub:release_benchmarking
+
+# Inside the container, build the applications in benchmarking mode
+./run build endoscopy_tool_tracking --benchmark
+./run build multiai_ultrasound --benchmark
+
+./run build release_benchmarking
+```
+
+Run the benchmarking script with no arguments to collect performance logs in the `./output` directory.
+```bash
+./run launch release_benchmarking
+```
+
+## Summarizing Data
+
+After running benchmarks, inside the dev environment, use `run launch` to process data statistics and create bar plot PNGs: 
+```bash
+./dev_container launch --img holohub:release_benchmarking
+./run launch release_benchmarking --extra_args "--process benchmarks/release_benchmarking"
+```
+
+Alternatively, collect results across platforms. On each machine:
+1. Run benchmarks:
+```bash
+./run launch release_benchmarking
+```
+2. Add platform configuration information:
+```bash
+./run launch release_benchmarking --extra_args "--print" > benchmarks/release_benchmarking/output/platform.txt
+```
+3. Transfer output contents from each platform to a single machine:
+```bash
+# Compress information for transfer
+pushd benchmarks/release_benchmarking
+tar cvf benchmarks-<platform-name>.tar.gz output/*
+
+# Migrate the results archive with your transfer tool of choice, such as SCP
+
+# Extract results to a subfolder on the target machine
+mkdir -p output/<release>/<platform-name>/
+pushd output/<release>/<platform-name>
+tar xvf benchmarks-<platform-name>
+```
+4. Use multiple `--process` flags to generate a batch of bar plots for multiple platform results:
+```bash
+./run launch release_benchmarking --extra_args "\
+    --process benchmarks/release_benchmarking/2.4/x86_64 \
+    --process benchmarks/release_benchmarking/2.4/IGX_iGPU \
+    --process benchmarks/release/benchmarking/2.4/IGX_dGPU"
+```
+
+## Presenting Data
+
+You can use the template markdown file in the [`template`](./template/) folder to generate a markdown
+or PDF report with benchmark data with `pandoc` and `Jinja2`.
+
+1. Copy and edit `template/release.json` with information about the benchmarking configuration, including
+the release version, platform configurations, and local paths to processed data. Run
+`./run launch` to print JSON-formatted platform details to the console about the current system:
+```bash
+./dev_container launch --img holohub:release_benchmarking
+./run launch release_benchmarking --extra_args "--print"
+```
+2. Render the document with the Jinja CLI tool:
+```bash
+pushd benchmarks/release_benchmarking
+jinja2 template/results.md.tmpl template/<release-version>.json --format=json > output/<release-version>.md
+```
+
+### (Optional) Generating a PDF report document
+
+You can convert the report to PDF format as an easy way to share your report as a single file
+with embedded plots.
+
+1. In your copy of `template/release.json`, update the `"format"` string to `"pdf"`.
+2. Follow the instructions above to generate your markdown report with Jinja2.
+3. Use `pandoc` to convert the markdown file to PDF:
+```bash
+pushd output
+pandoc <release-version>.md -o <release-version>.pdf --toc
+```
+
+### (Optional) Submitting Results to HoloHub
+
+The Holoscan SDK team may submit release benchmarking reports to HoloHub git history for general visibility. We use Markdown formatting to make plot diagrams accessible for direct download.
+
+1. Move `<release-version>.md` and accompanying plots to a new `release/<version>` folder.
+2. Update image paths in `<release-version.md>` and verify locally with a markdown renderer such as VS Code.
+3. Commit changes, push to GitHub, and open a Pull Request.
+
+## Cleanup
+Benchmarking changes to application YAML files can be discarded after benchmarks complete.
+```bash
+git checkout applications/*.yaml
+```
+
+## Troubleshooting
+
+__Why am I seeing high end-to-end latency spikes as outliers in my data?__
+
+Latency spikes may occur in display-driven benchmarking if the display goes to sleep. Please configure your
+display settings to prevent the display from going to sleep before running benchmarks.
+
+We have also infrequently observed latency spikes in cases where display drivers and CUDA Toolkit
+versions are not matched, and due to suboptimal GPU task preemption policies. We are still investigating these issues.
+
+__Benchmark applications are failing silently without writing log files.__
+
+Silent failures may indicate an issue with the underlying applications undergoing benchmarking.
+Try running the applications directly and verify execution is as expected:
+- `./run launch endoscopy_tool_tracking cpp`
+- `./run launch multiai_ultrasound cpp`
+
+In some cases you may need to clear your HoloHub build or data folders to address errors:
+- `./run clear_cache`
+- `rm -rf ./data`
+
+## Developer References
+
+While this tutorial is tailored to curated configurations of the Endoscopy Tool Tracking and
+Multi-AI Ultrasound HoloHub applications, developers utilize underlying
+Holoscan data frame flow tracking tools to similarly measure and analyze performance in custom
+Holoscan applications.
+
+- Refer to the [Holoscan Flow Benchmarking](../holoscan_flow_benchmarking/) project for general
+Holoscan performance profiling tools for both C++ and Python applications.
+- Refer to the [Holoscan Flow Benchmarking whitepaper](https://developer.download.nvidia.com/holoscan/Holoscan-Flow-Benchmarking.pdf) and [tutorial](../holoscan_flow_benchmarking/flow_benchmarking_tutorial.md) for a comprehensive overview of pipeline profiling tools.
+- Refer to [`run_benchmarks.sh`](./run_benchmarks.sh) for additional examples demonstrating
+performance data collection and reporting with Holoscan Flow Tracking scripts.
---
+++ benchmarks/release_benchmarking/run_benchmarks.sh
@@ -0,0 +1,334 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -e
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+TOP=$(realpath "${SCRIPT_DIR}/../..")
+FLOW_BENCHMARKING_DIR=${TOP}/benchmarks/holoscan_flow_benchmarking
+
+run_command() {
+    local status=0
+    local cmd="$*"
+
+    if [ "${DO_DRY_RUN}" != "true" ]; then
+        echo -e "${YELLOW}[command]${NOCOLOR} ${cmd}"
+    else
+        echo -e "${YELLOW}[dryrun]${NOCOLOR} ${cmd}"
+    fi
+
+    [ "$(echo -n "$@")" = "" ] && return 1 # return 1 if there is no command available
+
+    if [ "${DO_DRY_RUN}" != "true" ]; then
+        eval "$@"
+        status=$?
+    fi
+    return $status
+}
+
+print_platform_details() {
+    echo "Platform details:"
+    
+    cpu_details=$(lscpu)
+    echo "\"cpu\": \"$(echo "$cpu_details" | grep "Model name" | cut -d':' -f2 | xargs)\","
+
+    echo "\"dgpu\": \"$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader | xargs)\","
+    echo "\"driver\": \"$(nvidia-smi --query-gpu=driver_version --format=csv,noheader | xargs)\","
+    echo "\"cuda\": \"$(nvidia-smi | grep "CUDA Version" | awk '{print $9}')\","
+
+    echo "\"os\": \"$(cat /etc/os-release | grep "PRETTY_NAME" | cut -d'"' -f2)\","
+}
+
+run_benchmark() {
+    ARGS=("$@")
+    local runs=3
+    local instances=3
+    local messages=1000
+    local scheduler=greedy
+    local output=""
+    local headless="false"
+    local realtime="false"
+    local headless_str="display"
+    local realtime_str="offline"
+    local app_config=""
+    local app=""
+
+    for i in "${!ARGS[@]}"; do
+        arg="${ARGS[i]}"
+        if [[ $skipnext == "1" ]]; then
+            skipnext=0
+        elif [[ "$arg" == "--help" ]]; then
+            echo "Usage: $0 [options]"
+            echo "Options:"
+            echo "  --app <str>         Application to benchmark"
+            echo "  --app_config <str>  Application configuration file"
+            echo "  --runs <int>        Number of runs"
+            echo "  --instances <int>   Number of instances"
+            echo "  --messages <int>    Number of messages"
+            echo "  --scheduler <str>   Scheduler to use"
+            echo "  --output <str>      Output directory"
+            echo "  --headless          Run in headless mode"
+            echo "  --realtime          Run in real-time mode"
+            exit 0
+        elif [ "$arg" = "--app" ]; then
+            app="${ARGS[i + 1]}"
+            skipnext=1
+        elif [ "$arg" = "--app_config" ]; then
+            app_config="${ARGS[i + 1]}"
+            skipnext=1
+        elif [ "$arg" = "--runs" ]; then
+            runs="${ARGS[i + 1]}"
+            skipnext=1
+        elif [ "$arg" = "--instances" ]; then
+            instances="${ARGS[i + 1]}"
+            skipnext=1
+        elif [ "$arg" = "--messages" ]; then
+            messages="${ARGS[i + 1]}"
+            skipnext=1
+        elif [ "$arg" = "--scheduler" ]; then
+            scheduler="${ARGS[i + 1]}"
+            skipnext=1
+        elif [ "$arg" = "--output" ]; then
+            output="${ARGS[i + 1]}"
+            skipnext=1
+        elif [ "$arg" = "--headless" ]; then
+            headless="true"
+        elif [ "$arg" = "--realtime" ]; then
+            realtime="true"
+        fi
+    done
+
+    if [[ -z "${app_config}" || ! -f "${app_config}" ]]; then
+        echo "No application configuration filepath specified with '--app_config'"
+        exit 1
+    fi
+    
+    if [[ "${headless}" = "true" ]]; then
+        headless_str="headless"
+    fi
+    if [[ "${realtime}" = "true" ]]; then
+        realtime_str="realtime"
+    fi
+    output=${output:-"${SCRIPT_DIR}/output/${app}_${runs}_${instances}_${messages}_${scheduler}_${headless_str}_${realtime_str}"}
+    mkdir -p $(dirname ${output})
+
+    sed -i "s/^  headless: .*/  headless: ${headless}/" ${APP_CONFIG_PATH}
+    sed -i "s/^  realtime: .*/  realtime: ${realtime}/" ${APP_CONFIG_PATH}
+
+    run_command python \
+        ${FLOW_BENCHMARKING_DIR}/benchmark.py \
+        -a ${app} \
+        -r ${runs} \
+        -i ${instances} \
+        -m ${messages} \
+        --sched ${scheduler} \
+        -d ${output}
+}
+
+plot_benchmark() {
+    ARGS=("$@")
+    local statistics_args="--max --avg --stddev --median --min --tail --flatness -p 90 95 99 99.9"
+    local log_pattern=""
+    local log_groups=""
+    local log_dir="${SCRIPT_DIR}"
+
+    for i in "${!ARGS[@]}"; do
+        arg="${ARGS[i]}"
+        if [[ $skipnext == "1" ]]; then
+            skipnext=0
+        elif [[ "$arg" == "--help" ]]; then
+            echo "Usage: $0 [options]"
+            echo "Options:"
+            echo "  --log_dir <str>     parent directory containing log \"output\" subdirectory"
+            echo "  --log_pattern <str> log pattern to search for"
+            echo "  --stats <str>       statistics arguments for analyze.py"
+            echo "  --help              Display this help message"
+            exit 0
+        elif [ "$arg" = "--log_pattern" ]; then
+            log_pattern="${ARGS[i + 1]}"
+            skipnext=1
+        elif [ "$arg" = "--log_dir" ]; then
+            log_dir="${ARGS[i + 1]}"
+            skipnext=1
+        elif [ "$arg" = "--stats" ]; then
+            statistics_args="${ARGS[i + 1]}"
+            skipnext=1
+        fi
+    done
+
+    if [[ -z "${log_pattern}" ]]; then
+        echo "No log pattern specified with '--log_pattern'"
+        exit 1
+    fi
+    app_name=$(echo "$log_pattern" | sed -E 's/_[0-9].*//')
+
+    counter=1
+    for log_directory in $(find ${log_dir}/output -name ${log_pattern} -type d | sort -d); do
+        log_groups+="-g "
+        for log_file in $(find ${log_directory} -name "logger_*"); do
+            log_groups+="$(realpath ${log_file}) "
+        done
+        log_groups+="Group${counter} "
+        counter=$((counter + 1))
+    done
+    if [[ -z "${log_groups}" ]]; then
+        echo "No logs found for pattern ${log_pattern}"
+        exit 1
+    fi
+
+    processing_dir="$(realpath ${log_dir})/processed/${log_pattern}"
+    mkdir -p ${processing_dir}
+
+    pushd ${processing_dir}
+    run_command python \
+        ${FLOW_BENCHMARKING_DIR}/analyze.py \
+        --save-csv \
+        --no-display-graphs \
+        ${statistics_args} \
+        ${log_groups}
+
+    for statistics_csv in $(find ${processing_dir} -name "*.csv"); do
+        run_command python \
+            ${FLOW_BENCHMARKING_DIR}/generate_bar_graph.py \
+            ${statistics_csv} \
+            --app "${app_name}" \
+            --title "''" \
+            --quiet
+    done
+    popd
+}
+
+benchmark_endoscopy_tool_tracking() {
+    APP_CONFIG_PATH=${TOP}/build/endoscopy_tool_tracking/applications/endoscopy_tool_tracking/cpp/endoscopy_tool_tracking.yaml
+
+    # Test fewer instances on IGX
+    if [ $(uname -m) = "aarch64" ]; then
+        instance_range=$(seq 1 3);
+    else
+        instance_range=$(seq 1 8);
+    fi
+
+    # Real time
+    for instances in ${instance_range}; do
+        run_benchmark \
+            --app endoscopy_tool_tracking \
+            --app_config ${APP_CONFIG_PATH} \
+            --instances ${instances} \
+            --realtime
+    done
+
+    # Real time disabled ("offline")
+    for instances in ${instance_range}; do
+        run_benchmark \
+            --app endoscopy_tool_tracking \
+            --app_config ${APP_CONFIG_PATH} \
+            --instances ${instances}
+    done
+
+    # Real time disabled ("offline") + visualizations disabled ("headless")
+    for instances in ${instance_range}; do
+        run_benchmark \
+            --app endoscopy_tool_tracking \
+            --app_config ${APP_CONFIG_PATH} \
+            --instances ${instances} \
+            --headless
+    done
+}
+
+benchmark_multiai_ultrasound() {
+    APP_CONFIG_PATH=${TOP}/build/multiai_ultrasound/applications/multiai_ultrasound/cpp/multiai_ultrasound.yaml
+
+    # Test fewer instances on IGX
+    if [ $(uname -m) = "aarch64" ]; then
+        instance_range=$(seq 1 2);
+    else
+        instance_range=$(seq 1 3);
+    fi
+
+    # Real time
+    for instances in ${instance_range}; do
+        run_benchmark \
+            --app multiai_ultrasound \
+            --app_config ${APP_CONFIG_PATH} \
+            --instances ${instances} \
+            --realtime
+    done
+
+    # Real time disabled + visualizations disabled
+    for instances in ${instance_range}; do
+        run_benchmark \
+            --app multiai_ultrasound \
+            --app_config ${APP_CONFIG_PATH} \
+            --instances ${instances} \
+            --headless
+    done
+}
+
+main() {
+    ARGS=("$@")
+    local process_only=false
+    local data_dirs=()
+
+    for i in "${!ARGS[@]}"; do
+        arg="${ARGS[i]}"
+        if [[ $skipnext == "1" ]]; then
+            skipnext=0;
+        elif [[ "$arg" == "--help" ]]; then
+            echo "Usage: $0 [options]"
+            echo "Options:"
+            echo "  --dryrun             Print commands without running them"
+            echo "  --print              Print platform details"
+            echo "  --process <path(s)>  Process prior output without running new benchmarks. Multiple --process values can be specified."
+            exit 0;
+        elif [[ "$arg" == "--dryrun" ]]; then
+            DO_DRY_RUN="true";
+        elif [[ "$arg" == "--print" ]]; then
+            set +x
+            print_platform_details
+            exit 0;
+        elif [[ "$arg" == "--process" ]]; then
+            data_dirs+=("${ARGS[i + 1]}")
+            process_only=true
+            skipnext=1;
+        fi
+    done
+
+    if [[ -z "${data_dirs}" ]]; then
+        data_dirs=("${SCRIPT_DIR}")
+    fi
+
+    if [[ ${process_only} = "false" ]]; then
+        pushd ${TOP}
+        benchmark_endoscopy_tool_tracking
+        benchmark_multiai_ultrasound
+        popd;
+    fi
+    
+    for data_dir in ${data_dirs[@]}; do
+        for pattern_suffix in "display_realtime" "display_offline" "headless_offline"; do
+            plot_benchmark \
+                --log_pattern "endoscopy_tool_tracking_3_[0-9]_1000_greedy_$pattern_suffix" \
+                --log_dir ${data_dir}
+        done
+
+        for pattern_suffix in "display_realtime" "headless_offline"; do
+            plot_benchmark \
+                --log_pattern "multiai_ultrasound_3_[0-9]_1000_greedy_$pattern_suffix" \
+                --log_dir ${data_dir}
+        done
+    done
+}
+
+main "$@"
