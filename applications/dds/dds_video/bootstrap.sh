3f342417db8577658a12590f212a599d3b80797d
Victor Chang <vicchang@nvidia.com>
2025-02-24 17:11:10 -0800

    Rewrite instructions with .run installer
    Signed-off-by: Victor Chang <vicchang@nvidia.com>

--- applications/dds/dds_video/bootstrap.sh
+++ applications/dds/dds_video/bootstrap.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-/host/bin/rtienv-7.3.0 -l /opt/rti.com/rti_license.dat >> /tmp/rtienv.sh
-chmod +x /tmp/rtienv.sh
-
-source /tmp/rtienv.sh
+source /opt/rti.com/rti_connext_dds-7.3.0/resource/scripts/rtisetenv_x64Linux4gcc7.3.0.bash
 exec /bin/bash
