65365e496b597f4e6edceb6f3c8464ff908fb66d
Mikael Brudfors <mbrudfors@nvidia.com>
2025-03-07 15:00:14 +0000

    lint fixes
    Signed-off-by: Mikael Brudfors <mbrudfors@nvidia.com>

--- applications/colonoscopy_segmentation/colonoscopy_segmentation.py
+++ applications/colonoscopy_segmentation/colonoscopy_segmentation.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import argparse
+import os
 
 import cupy as cp
 from cupyx.scipy import ndimage
@@ -263,7 +263,7 @@ if __name__ == "__main__":
         help=("Set the data path"),
     )
     parser.add_argument(
-        '--contours',
+        "--contours",
         default=False,
         action=argparse.BooleanOptionalAction,
         help=("Show segmentation contours instead of mask (default: False)"),
