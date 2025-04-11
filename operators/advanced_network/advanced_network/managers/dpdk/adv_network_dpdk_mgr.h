b9b028418cc64826b43ce403ceb62c597238eb11
Alexis Girault <agirault@nvidia.com>
2025-04-11 13:34:10 -0400

    adv_net: overrides for get_rx_burst
    Loop over port or queues
    
    Signed-off-by: Alexis Girault <agirault@nvidia.com>

--- operators/advanced_network/advanced_network/managers/dpdk/adv_network_dpdk_mgr.h
+++ operators/advanced_network/advanced_network/managers/dpdk/adv_network_dpdk_mgr.h
@@ -190,6 +190,7 @@ class DpdkMgr : public Manager {
   void free_tx_burst(BurstParams* burst) override;
 
   Status get_rx_burst(BurstParams** burst, int port, int q) override;
+  using holoscan::advanced_network::Manager::get_rx_burst;  // for overloads
   Status set_packet_tx_time(BurstParams* burst, int idx, uint64_t timestamp);
   void free_rx_metadata(BurstParams* burst) override;
   void free_tx_metadata(BurstParams* burst) override;
