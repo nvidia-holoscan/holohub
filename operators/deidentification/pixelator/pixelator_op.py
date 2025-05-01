c1fd0fc8cbe2076ffa0a72dfca5895b7e9c572ad
B Hashemian <3968947+bhashemian@users.noreply.github.com>
2025-05-01 13:14:47 -0400

    updat docstring for PixelatorOp
    Signed-off-by: B Hashemian <3968947+bhashemian@users.noreply.github.com>

--- operators/deidentification/pixelator/pixelator_op.py
+++ operators/deidentification/pixelator/pixelator_op.py
@@ -1,5 +1,17 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import cupy as cp
 from holoscan.core import Operator, OperatorSpec
@@ -10,6 +22,11 @@ __all__ = ["PixelatorOp"]
 class PixelatorOp(Operator):
     """
     This operator is used to deidentify the input image by pixelation.
+
+    Args:
+        block_size_h (int): The height of the pixelation block.
+        block_size_w (int): The width of the pixelation block.
+        tensor_name (str): The name of the tensor to be pixelated.
     """
 
     def __init__(
