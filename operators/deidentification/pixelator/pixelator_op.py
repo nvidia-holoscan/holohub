7ee6bfc86d5745525dcccfb50b6bdf1cc3aa17c9
B Hashemian <3968947+bhashemian@users.noreply.github.com>
2025-05-02 17:45:47 -0400

    add support for non-divisible pixelation block sizes
    Signed-off-by: B Hashemian <3968947+bhashemian@users.noreply.github.com>

--- operators/deidentification/pixelator/pixelator_op.py
+++ operators/deidentification/pixelator/pixelator_op.py
@@ -37,6 +37,10 @@ class PixelatorOp(Operator):
         tensor_name: str = "",
         **kwargs,
     ):
+        if block_size_h <= 0:
+            raise ValueError("block_size_h must be a positive integer")
+        if block_size_w <= 0:
+            raise ValueError("block_size_w must be a positive integer")
         self.block_size_h = block_size_h
         self.block_size_w = block_size_w
         self.tensor_name = tensor_name
@@ -48,20 +52,37 @@ class PixelatorOp(Operator):
 
     def compute(self, op_input, op_output, context):
         in_message = op_input.receive("in")
+        if self.tensor_name not in in_message:
+            raise KeyError(f"Tensor '{self.tensor_name}' not found in input message")
         image = cp.asarray(in_message[self.tensor_name])
-        # Pixelate the image by downsampling and upsampling
         h, w = image.shape[:2]
-        small_h = h // self.block_size_h
-        small_w = w // self.block_size_w
-        # Reshape and mean across blocks to downsample
-        reshaped = image.reshape(small_h, self.block_size_h, small_w, self.block_size_w, -1)
-        downsampled = cp.mean(reshaped, axis=(1, 3))
-        # Repeat each pixel to upsample back to original size
-        upsampled = cp.repeat(
-            cp.repeat(downsampled, self.block_size_h, axis=0), self.block_size_w, axis=1
+
+        # Pad image to make dimensions divisible by block size
+        pad_h = (self.block_size_h - h % self.block_size_h) % self.block_size_h
+        pad_w = (self.block_size_w - w % self.block_size_w) % self.block_size_w
+        if pad_h > 0 or pad_w > 0:
+            pad_width = ((0, pad_h), (0, pad_w)) + ((0, 0),) * (image.ndim - 2)
+            image_padded = cp.pad(image, pad_width, mode="constant", constant_values=0)
+        else:
+            image_padded = image
+
+        H, W = image_padded.shape[:2]
+
+        # Reshape and mean across blocks
+        reshaped = image_padded.reshape(
+            H // self.block_size_h, self.block_size_h, W // self.block_size_w, self.block_size_w, -1
         )
-        image = upsampled.astype(cp.uint8)
-        # add the holoviz tensors to the output message
+        pixel_blocks = cp.mean(reshaped, axis=(1, 3))
+
+        # Repeat each block to create pixelated image of original size
+        pixelated = cp.repeat(
+            cp.repeat(pixel_blocks, self.block_size_h, axis=0), self.block_size_w, axis=1
+        )
+
+        # Crop to original size if padded
+        image = pixelated[:h, :w, ...].astype(cp.uint8)
+
+        # Create and emit the result
         out_message = in_message.copy()
         out_message[self.tensor_name] = image
         op_output.emit(out_message, "out")
