ec2f283983bf5efbb72bfada884ac02319e73222
Wenqi Li <wenqil@nvidia.com>
2025-06-19 07:26:07 +0100

    update
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- utilities/cli/tests/test_cli.py
+++ utilities/cli/tests/test_cli.py
@@ -212,7 +212,7 @@ class TestHoloHubCLI(unittest.TestCase):
 
         # Verify the container.run was called with the correctly quoted arguments
         kwargs = mock_container.run.call_args[1]
-        cmd_string = kwargs["extra_args"][1]
+        cmd_string = kwargs["extra_args"][0]
         self.assertIn("./holohub run test_project", cmd_string)
         self.assertIn("--run-args", cmd_string)
         self.assertIn(quoted_value, cmd_string)
@@ -408,7 +408,7 @@ class TestHoloHubCLI(unittest.TestCase):
         # Verify container build
         mock_container.run.assert_called()
         kwargs = mock_container.run.call_args[1]
-        command_string = kwargs["extra_args"][1]
+        command_string = kwargs["extra_args"][0]
         self.assertIn(f'--build-with "{operators}"', command_string)
         mock_container.reset_mock()
 
@@ -418,7 +418,7 @@ class TestHoloHubCLI(unittest.TestCase):
         # Verify container run
         mock_container.run.assert_called()
         kwargs = mock_container.run.call_args[1]
-        command_string = kwargs["extra_args"][1]
+        command_string = kwargs["extra_args"][0]
         self.assertIn(f'--build-with "{operators}"', command_string)
 
     @patch("utilities.cli.holohub.HoloHubCLI.find_project")
@@ -609,7 +609,7 @@ exec {holohub_script} "$@"
         mock_container.build.assert_called_once()
         mock_container.run.assert_called_once()
         kwargs = mock_container.run.call_args[1]
-        command_string = kwargs["extra_args"][1]
+        command_string = kwargs["extra_args"][0]
         self.assertIn("./holohub install test_project --local", command_string)
         self.assertIn("--build-type debug", command_string)
         self.assertIn("--language cpp", command_string)
@@ -617,7 +617,7 @@ exec {holohub_script} "$@"
         args = self.cli.parser.parse_args("install test_project --parallel 4".split())
         args.func(args)
         kwargs = mock_container.run.call_args[1]
-        command_string = kwargs["extra_args"][1]
+        command_string = kwargs["extra_args"][0]
         self.assertIn("--parallel 4", command_string)
 
     @patch("utilities.cli.holohub.HoloHubCLI.find_project")
@@ -740,7 +740,7 @@ exec {holohub_script} "$@"
 
             mock_container.build.assert_called_once()
             mock_container.run.assert_called_once()
-            command_string = mock_container.run.call_args[1]["extra_args"][1]
+            command_string = mock_container.run.call_args[1]["extra_args"][0]
             self.assertIn("--benchmark", command_string)
             self.assertIn("./holohub build test_app --local", command_string)
 
