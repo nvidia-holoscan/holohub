40ff955fd81527d223c1db500b01e582fb7bd70b
Julien Jomier <219040+jjomier@users.noreply.github.com>
2025-07-03 12:37:53 -0400

    Improve docker for colonoscopy application (#939)
    * Improve docker for colonoscopy application
    
    Signed-off-by: Julien Jomier <jjomier@nvidia.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Tom Birdsong <40648863+tbirdso@users.noreply.github.com>
    Co-authored-by: Alexis Girault <agirault@users.noreply.github.com>
    Signed-off-by: Julien Jomier <219040+jjomier@users.noreply.github.com>
    
    * Remove lazy import and improve python installation in dockerfile
    
    * Fix linting errors
    
    Signed-off-by: Julien Jomier <jjomier@nvidia.com>
    
    ---------
    
    Signed-off-by: Julien Jomier <jjomier@nvidia.com>
    Signed-off-by: Julien Jomier <219040+jjomier@users.noreply.github.com>
    Co-authored-by: Tom Birdsong <40648863+tbirdso@users.noreply.github.com>
    Co-authored-by: Alexis Girault <agirault@users.noreply.github.com>
    Signed-off-by: Vani Nagarajan <vanin@nvidia.com>

--- applications/colonoscopy_segmentation/colonoscopy_segmentation.py
+++ applications/colonoscopy_segmentation/colonoscopy_segmentation.py
@@ -28,8 +28,6 @@ from holoscan.operators import (
 )
 from holoscan.resources import BlockMemoryPool, CudaStreamPool, MemoryStorageType
 
-from holohub.aja_source import AJASourceOp
-
 
 class ContourOp(Operator):
     """Operator to format input image for inference"""
@@ -120,6 +118,8 @@ class ColonoscopyApp(Application):
 
         is_aja = self.source.lower() == "aja"
         if is_aja:
+            from holohub.aja_source import AJASourceOp
+
             source = AJASourceOp(self, name="aja", **self.kwargs("aja"))
             drop_alpha_block_size = 1920 * 1080 * n_channels * bpp
             drop_alpha_num_blocks = 2
