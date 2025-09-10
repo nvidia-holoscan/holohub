07a48c850a5bab5f9773ebc293e08a163cdba83e
cdinea <cdinea@nvidia.com>
2025-09-10 01:23:41 -0700

    fix: Add copyright header to video streaming server run_test.sh
    Signed-off-by: cdinea <cdinea@nvidia.com>

--- applications/video_streaming/video_streaming_server/cpp/run_test.sh
+++ applications/video_streaming/video_streaming_server/cpp/run_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 
 # Streaming Server Demo C++ Test Script
 set -e
