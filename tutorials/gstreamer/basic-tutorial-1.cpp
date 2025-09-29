ddb44648b2404568dfaf30b4f9bbe2c4e5a391c0
Gil Shapira <gshapira@nvidia.com>
2025-09-29 17:28:06 +0000

    tutorials

---
+++ tutorials/gstreamer/basic-tutorial-1.cpp
@@ -0,0 +1,55 @@
+#include <gst/gst.h>
+#include "../../../operators/gstreamer/core/initializer.hpp"
+
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#endif
+
+int
+tutorial_main (int argc, char *argv[])
+{
+  GstElement *pipeline;
+  GstBus *bus;
+  GstMessage *msg;
+
+  /* Initialize GStreamer using GstInitializer */
+  holoscan::ops::GstInitializer gst_initializer;
+
+  /* Build the pipeline */
+  pipeline =
+      gst_parse_launch
+      ("playbin uri=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm",
+      NULL);
+
+  /* Start playing */
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+
+  /* Wait until error or EOS */
+  bus = gst_element_get_bus (pipeline);
+  msg =
+      gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE,
+      static_cast<GstMessageType>(GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
+
+  /* See next tutorial for proper error message handling/parsing */
+  if (GST_MESSAGE_TYPE (msg) == GST_MESSAGE_ERROR) {
+    g_printerr ("An error occurred! Re-run with the GST_DEBUG=*:WARN "
+        "environment variable set for more details.\n");
+  }
+
+  /* Free resources */
+  gst_message_unref (msg);
+  gst_object_unref (bus);
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (pipeline);
+  return 0;
+}
+
+int
+main (int argc, char *argv[])
+{
+#if defined(__APPLE__) && TARGET_OS_MAC && !TARGET_OS_IPHONE
+  return gst_macos_main ((GstMainFunc) tutorial_main, argc, argv, NULL);
+#else
+  return tutorial_main (argc, argv);
+#endif
+}
