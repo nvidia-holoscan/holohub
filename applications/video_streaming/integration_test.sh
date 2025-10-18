5fbdb900ed7c9a35bc579da4561d1b78b2fca571
cdinea <cdinea@nvidia.com>
2025-10-17 19:49:09 -0700

    fix: remove leading space from shebang in integration_test.sh
    Signed-off-by: cdinea <cdinea@nvidia.com>

--- applications/video_streaming/integration_test.sh
+++ applications/video_streaming/integration_test.sh
@@ -1,4 +1,4 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
