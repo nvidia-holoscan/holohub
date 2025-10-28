79fd5dff605fafbd880c226d53c1a6e7377d66b9
Cliff Burdick <30670611+cliffburdick@users.noreply.github.com>
2025-10-28 08:46:14 -0700

    Update adv_network_dpdk_mgr.h
    Signed-off-by: Cliff Burdick <30670611+cliffburdick@users.noreply.github.com>
    Signed-off-by: Cliff Burdick <cburdick@nvidia.com>
    Signed-off-by: B Hashemian <3968947+bhashemian@users.noreply.github.com>

--- operators/advanced_network/advanced_network/managers/dpdk/adv_network_dpdk_mgr.h
+++ operators/advanced_network/advanced_network/managers/dpdk/adv_network_dpdk_mgr.h
@@ -1,9 +1,5 @@
 /*
-<<<<<<< HEAD
  * SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
-=======
- * SPDX-FileCopyrightText: Copyright (c) 2023-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
->>>>>>> 332dabe5 (Arbitrary byte matching with flex parser)
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
