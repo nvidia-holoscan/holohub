8013f42d2b1821545d35829a5f858bfc01e3f5ff
B Hashemian <3968947+bhashemian@users.noreply.github.com>
2025-11-21 12:55:39 -0500

    move the copyright to the top
    Signed-off-by: B Hashemian <3968947+bhashemian@users.noreply.github.com>

--- applications/surgical_scene_recon/tests/test_static_rendering.py
+++ applications/surgical_scene_recon/tests/test_static_rendering.py
@@ -1,8 +1,3 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -17,6 +12,11 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
 
 """
 Test Static Rendering - Phase 1.2a
