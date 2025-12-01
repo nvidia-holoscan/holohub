c94aff8874ec52a22e7e2d9961ee89803ef9eebd
eagostini <eagostini@nvidia.com>
2025-12-01 10:52:17 -0800

    Fix code style
    Signed-off-by: eagostini <eagostini@nvidia.com>

--- operators/advanced_network/advanced_network/managers/gpunetio/adv_network_doca_kernels.h
+++ operators/advanced_network/advanced_network/managers/gpunetio/adv_network_doca_kernels.h
@@ -1,6 +1,6 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2023-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
- * SPDX-License-Identifier: Apache-2.0
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2025 NVIDIA CORPORATION & AFFILIATES. All rights
+ * reserved. SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,9 +32,8 @@ doca_error_t doca_receiver_packet_kernel(cudaStream_t stream, int rxqn, uintptr_
 
 doca_error_t doca_sender_packet_kernel(cudaStream_t stream, struct doca_gpu_eth_txq* txq,
                                        uint64_t pkt_buff_addr, const uint32_t pkt_buff_mkey,
-                                       uint32_t gpu_pkt0_idx,
-                                       const size_t num_pkts, uint32_t max_pkts,
-                                       const uint64_t max_pkt_size,
+                                       uint32_t gpu_pkt0_idx, const size_t num_pkts,
+                                       uint32_t max_pkts, const uint64_t max_pkt_size,
                                        uint32_t* gpu_pkts_len, bool set_completion);
 #if __cplusplus
 }
