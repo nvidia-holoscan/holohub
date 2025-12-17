7c8ecaff32712fcd3c402fa63bebc70b16bdb088
Cliff Burdick <cburdick@nvidia.com>
2025-12-17 23:38:26 +0000

    Update pragma

--- applications/adv_networking_bench/cpp/rdma_bench.h
+++ applications/adv_networking_bench/cpp/rdma_bench.h
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#pragma once 
+#pragma once
 
 #include "holoscan/holoscan.hpp"
 #include <queue>
