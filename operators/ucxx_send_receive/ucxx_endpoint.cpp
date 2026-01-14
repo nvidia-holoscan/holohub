1bee99e861dd9221d2d68536f6406c9907b0c347
Tom Birdsong <tbirdsong@nvidia.com>
2026-01-13 21:10:47 -0500

    Address PR comments
    - Fix linting
    - Fix copyrights
    - Fix application metadata.json fields
    - Fix header
    
    Signed-off-by: Tom Birdsong <tbirdsong@nvidia.com>

--- operators/ucxx_send_receive/ucxx_endpoint.cpp
+++ operators/ucxx_send_receive/ucxx_endpoint.cpp
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -43,7 +43,8 @@ void UcxxEndpoint::setup(holoscan::ComponentSpec& spec) {
 }
 
 void UcxxEndpoint::on_connection_request(ucp_conn_request_h conn_request) {
-  auto ep = listener_->createEndpointFromConnRequest(conn_request, /*endpoint_error_handling=*/true);
+  auto ep = listener_->createEndpointFromConnRequest(conn_request,
+    /*endpoint_error_handling=*/true);
   std::atomic_store(&endpoint_, ep);
   HOLOSCAN_LOG_INFO("Endpoint connected");
 
