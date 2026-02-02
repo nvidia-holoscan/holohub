77a43c2bd08d25d17f9a5b94c1b8e0e6c1598bc6
Dana Lessner <dlessner@nvidia.com>
2026-02-02 11:25:36 +0000

    Update copyright year to 2025-2026 in various files
    Signed-off-by: Dana Lessner <dlessner@nvidia.com>

--- applications/adv_networking_media_sender/python/adv_networking_media_sender.py
+++ applications/adv_networking_media_sender/python/adv_networking_media_sender.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# SPDX-FileCopyrightText: Copyright (c) 2022-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
