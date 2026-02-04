469ba4059d293f40c21b37e96405af1f0b59579f
B Hashemian <3968947+bhashemian@users.noreply.github.com>
2026-02-04 13:45:08 -0500

    add copyright and fix cpp linting
    Signed-off-by: B Hashemian <3968947+bhashemian@users.noreply.github.com>

--- applications/holocat/cpp/hc_data_rx_op.cpp
+++ applications/holocat/cpp/hc_data_rx_op.cpp
@@ -1,9 +1,26 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved. SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * @file hc_data_rx_op.cpp
  * @brief HoloCat Data Receive Operator
- * 
+ *
  * HoloCat Data Receive Operator
- * 
+ *
  * This file implements the HcDataRxOp operator that receives counter data
  * for testing and demonstration purposes.
  */
@@ -18,14 +35,13 @@ namespace holocat {
 void HcDataRxOp::setup(holoscan::OperatorSpec& spec) {
   // Configure input port for receiving counter data
   spec.input<int>("count_in");
-  
+
   HOLOSCAN_LOG_INFO("HcDataRxOp: Setup complete - configured input port 'count_in'");
 }
 
-void HcDataRxOp::compute(holoscan::InputContext& op_input, 
-                         holoscan::OutputContext& op_output, 
+void HcDataRxOp::compute(holoscan::InputContext& op_input,
+                         holoscan::OutputContext& op_output,
                          holoscan::ExecutionContext& context) {
-
   // Receive count value from ECat bus
   auto maybe_count = op_input.receive<int>("count_in");
   if (!maybe_count) {
@@ -36,6 +52,6 @@ void HcDataRxOp::compute(holoscan::InputContext& op_input,
   HOLOSCAN_LOG_INFO("HcDataRxOp: Received count: {}", last_count_);
 }
 
-} // namespace holocat
+}  // namespace holocat
 
 /*-END OF SOURCE FILE--------------------------------------------------------*/
--- applications/holocat/cpp/holocat_config.hpp
+++ applications/holocat/cpp/holocat_config.hpp
@@ -1,7 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved. SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * @file holocat_config.hpp
  * @brief HoloCat application and EtherCAT configuration struct
- * 
+ *
  * Defines the HolocatConfig structure used for configuring EtherCAT
  * and HoloCat integration within the NVIDIA Holoscan framework.
  */
@@ -14,23 +31,23 @@
 
 namespace holocat {
 
-  // Configuration structure for Holoscan integration
+// Configuration structure for Holoscan integration
 struct HolocatConfig {
-    std::string adapter_name;
-    std::string eni_file;
-    uint32_t rt_priority;
-    uint32_t job_thread_priority;
-    bool enable_rt;
-    uint32_t dio_out_offset;
-    uint32_t dio_in_offset;
-    uint32_t max_acyc_frames;
-    uint32_t job_thread_stack_size;
-    std::string log_level;
-    uint64_t cycle_time_us;
-    // For validation error reporting
-    std::string error_message;
+  std::string adapter_name;
+  std::string eni_file;
+  uint32_t rt_priority;
+  uint32_t job_thread_priority;
+  bool enable_rt;
+  uint32_t dio_out_offset;
+  uint32_t dio_in_offset;
+  uint32_t max_acyc_frames;
+  uint32_t job_thread_stack_size;
+  std::string log_level;
+  uint64_t cycle_time_us;
+  // For validation error reporting
+  std::string error_message;
 };
 
-}
+}  // namespace holocat
 #endif /* INC_HOLOCAT_CONFIG_H */
 
