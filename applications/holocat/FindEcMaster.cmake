08f735227d24a0c31a5af077a6c6ff2a82ca6590
B Hashemian <3968947+bhashemian@users.noreply.github.com>
2026-02-04 14:03:50 -0500

    update missing copyright
    Signed-off-by: B Hashemian <3968947+bhashemian@users.noreply.github.com>

--- applications/holocat/FindEcMaster.cmake
+++ applications/holocat/FindEcMaster.cmake
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES.
+# All rights reserved. SPDX-License-Identifier: Apache-2.0
+#
 # FindEcMaster.cmake
 # Find the acontis EC-Master SDK
 #
