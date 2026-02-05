663465a6f85aab6e61af143132a2455109b4f739
H. Hawkeye King <hking@nvidia.com>
2026-02-05 13:28:44 -0800

    Add license headers to tests

--- applications/holocat/tests/test_integration_hardware.cpp
+++ applications/holocat/tests/test_integration_hardware.cpp
@@ -1,3 +1,20 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved. SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * @file test_integration_hardware.cpp
  * @brief Hardware Integration Test for HoloCat
@@ -95,7 +112,7 @@ class LoopbackCheckRxOp : public holoscan::Operator {
  public:
   HOLOSCAN_OPERATOR_FORWARD_ARGS(LoopbackCheckRxOp);
   LoopbackCheckRxOp() = default;
-  void setup(holoscan::OperatorSpec& spec) { spec.input<int>("count_in"); };
+  void setup(holoscan::OperatorSpec& spec) { spec.input<int>("count_in"); }
   void compute(holoscan::InputContext& op_input, holoscan::OutputContext& op_output,
                holoscan::ExecutionContext& context) {
     auto maybe_count = op_input.receive<int>("count_in");
@@ -104,7 +121,7 @@ class LoopbackCheckRxOp : public holoscan::Operator {
     } else {
       HOLOSCAN_LOG_ERROR("LoopbackCheckRxOp: Failed to receive count from ECat bus");
     }
-  };
+  }
 };
 
 /**
